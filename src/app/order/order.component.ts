821cc000ad764b2f2c38d082e9f382260b31c839
Firas <firasaydi0@gmail.com>
2024-04-22 23:03:50 +0100

    modification

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -13,15 +13,24 @@ export class OrderComponent implements OnInit {
   ngOnInit(): void {
     // Récupérer le menu depuis le localStorage
     const storedMenu = localStorage.getItem('currentMenu');
-    console.log("storedMenu: ",storedMenu)
     if (storedMenu !== null) {
       const decodedMenu = decodeURIComponent(storedMenu);
       this.menu = JSON.parse(decodedMenu);
-      console.log("menu: ",this.menu)
+      // Initialiser la quantité de chaque plat à 1
+      this.menu.plats.forEach((plat: { quantity: number; }) => plat.quantity = 0);
     } else {
       // Gérer le cas où le menu n'est pas disponible dans le localStorage
       console.error("Le menu n'est pas disponible dans le localStorage.");
     }
   }
 
+  incrementQuantity(index: number) {
+    this.menu.plats[index].quantity++;
+  }
+
+  decrementQuantity(index: number) {
+    if (this.menu.plats[index].quantity > 0) {
+      this.menu.plats[index].quantity--;
+    }
+  }
 }
