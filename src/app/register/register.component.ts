fd7582dc3b018c7c63d22809a6a9313718537d71
Firas <firasaydi0@gmail.com>
2024-05-08 13:21:52 +0100

    modification

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -87,7 +87,7 @@ export class RegisterComponent implements OnInit {
               longitude: data.longitude,
               reviews: [
                 {
-                  text: 'Delicious chicken',
+                  text: 'Delicious, I like this restaurant',
                 },
               ],
             };
